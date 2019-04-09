2e5748436bdf9598303b80fcd7d9872088930d68
TheBigL <lmohamed1@studentmail.nait.ca>
2019-04-09 11:52:03 -0600

    Building the Queue.

--- src/Queue.java
+++ src/Queue.java
@@ -1,4 +1,59 @@
 public class Queue
 {
+    private static class Node
+    {
+        private int data;
+        private Node next;
 
+        private Node(int data)
+        {
+            this.data = data;
+        }
+
+    }
+
+    private Node head;
+
+    private Node tail;
+
+    public boolean isEmpty()
+    {
+        return head == null;
+    }
+
+    public int peek(int data)
+    {
+        return head.data;
+    }
+
+    public void add(int data)
+    {
+        Node newNode = new Node(data);
+
+        if(tail != null)
+        {
+            tail.next = newNode;
+        }
+
+        tail = newNode;
+        if(head == null)
+        {
+            head = newNode;
+
+        }
+    }
+
+    public int remove()
+    {
+        int data = head.data;
+        head = head.next;
+
+        if(head == null)
+        {
+            tail = null;
+        }
+
+        return data;
+
+    }
 }
--- src/Stack.java
+++ src/Stack.java
@@ -1,4 +1,18 @@
 public class Stack
 {
 
+    private static class Node
+    {
+        private int data;
+        private Node next;
+
+        private Node(int data)
+        {
+            this.data = data;
+        }
+
+    }
+
+
+
 }
